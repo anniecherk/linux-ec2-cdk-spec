dd9cc5b26dee4d117ca7fdfb7a371b341829998a
Annie Cherkaev <annie.cherk@gmail.com>
2024-09-03 21:11:32 -0600

    init setup that actually starts a tiny linux machine

---
+++ Justfile
@@ -0,0 +1,30 @@
+
+# first time set up:
+#
+# template was gen'd w:
+# cdk init --language=python
+#
+# source .venv/bin/activate
+# pip install -r requirements.txt
+# cdk bootstrap
+# 
+# install session manager plugin on macos
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/install-plugin-macos-overview.html
+
+
+# it's python don't forget
+# source .venv/bin/activate
+
+# run whenever you want to change the infra
+deploy:
+    cdk deploy
+
+connect: 
+    #!/bin/zsh
+    instanceID=$(aws cloudformation describe-stacks --stack-name AwsEc2TinyLinuxCdkSpecStack --query "Stacks[0].Outputs[?OutputKey=='InstanceId'].OutputValue" --output text)
+    aws ssm start-session --target $instanceID
+
+# destroys the instance & vpc & iam role & everything else
+# if u don't do this u'll keep getting billed for all the resources being up
+destroy:
+    cdk destroy
--- aws_ec2_tiny_linux_cdk_spec/aws_ec2_tiny_linux_cdk_spec_stack.py
+++ aws_ec2_tiny_linux_cdk_spec/aws_ec2_tiny_linux_cdk_spec_stack.py
@@ -1,8 +1,10 @@
 from aws_cdk import (
-    # Duration,
+    CfnOutput,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_iam as iam,
 )
+
 from constructs import Construct
 
 class AwsEc2TinyLinuxCdkSpecStack(Stack):
@@ -10,10 +12,45 @@ class AwsEc2TinyLinuxCdkSpecStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Define the VPC
+        vpc = ec2.Vpc(
+            self, "ec2-tiny-linux-vpc",
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsEc2TinyLinuxCdkSpecQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the Security Group
+        security_group = ec2.SecurityGroup(
+            self, "InstanceSecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True,
+            description="Allow outbound traffic to vpc, no open ports: we will use SSM for access",
+        )
+
+        # Define the IAM Role for SSM
+        role = iam.Role(
+            self, "InstanceSSMRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"),
+            ]
+        )
+
+        startup_commands = ec2.UserData.for_linux()
+        startup_commands.add_commands(
+            "sudo yum update -y",
+            "sudo yum install -y zsh",
+            "chsh -s /bin/zsh ssm-user"
+        )
+
+        # Define the EC2 instance
+        instance = ec2.Instance(
+            self, "MyInstance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.latest_amazon_linux2(),
+            vpc=vpc,
+            security_group=security_group,
+            role=role,
+            user_data=startup_commands
+        )
+
+        # Output the Instance ID upon deployment
+        CfnOutput(self, "InstanceId", value=instance.instance_id)
